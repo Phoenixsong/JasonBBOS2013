0abbde4fb9e0bd3bc18eff9de6668b219dc77dc3
Phoenixsong <phoenixkindred@gmail.com>
2013-09-07 20:42:07 -0400

    Enhanced the keyboard driver: punctuation chars

--- scripts/os/deviceDriverKeyboard.js
+++ scripts/os/deviceDriverKeyboard.js
@@ -51,7 +51,117 @@ function krnKbdDispatchKeyPress(params)
                (keyCode == 32)                     ||   // space
                (keyCode == 13) )                        // enter
     {
-        chr = String.fromCharCode(keyCode);
+		if (isShifted){
+			if (keyCode == 49){
+				chr = "!";
+			}
+			else if (keyCode == 50){
+				chr = "@";
+			}
+			else if (keyCode == 51){
+				chr = "#";
+			}
+			else if (keyCode == 52){
+				chr = "$";
+			}
+			else if (keyCode == 53){
+				chr = "%";
+			}
+			else if (keyCode == 54){
+				chr = "^";
+			}
+			else if (keyCode == 55){
+				chr = "&";
+			}
+			else if (keyCode == 56){
+				chr = "*";
+			}
+			else if (keyCode == 57){
+				chr = "(";
+			}
+			else if (keyCode == 48){
+				chr = ")";
+			}
+			else{
+				chr = String.fromCharCode(keyCode);
+			}
+		}
+		else{
+			chr = String.fromCharCode(keyCode);
+		}
         _KernelInputQueue.enqueue(chr); 
     }
+	else if ((keyCode >= 186 && keyCode <= 192) || (keyCode >= 219 && keyCode <= 222)){
+		if (isShifted){
+			if (keyCode == 186){
+				chr = ":";
+			}
+			else if (keyCode == 187){
+				chr = "+";
+			}
+			else if (keyCode == 188){
+				chr = "<";
+			}
+			else if (keyCode == 189){
+				chr = "_";
+			}
+			else if (keyCode == 190){
+				chr = ">";
+			}
+			else if (keyCode == 191){
+				chr = "?";
+			}
+			else if (keyCode == 192){
+				chr = "~";
+			}
+			else if (keyCode == 219){
+				chr = "{";
+			}
+			else if (keyCode == 220){
+				chr = "|";
+			}
+			else if (keyCode == 221){
+				chr = "}";
+			}
+			else if (keyCode == 222){
+				chr = '"';
+			}
+		}
+		else{
+			if (keyCode == 186){
+				chr = ";";
+			}
+			else if (keyCode == 187){
+				chr = "=";
+			}
+			else if (keyCode == 188){
+				chr = ",";
+			}
+			else if (keyCode == 189){
+				chr = "-";
+			}
+			else if (keyCode == 190){
+				chr = ".";
+			}
+			else if (keyCode == 191){
+				chr = "/";
+			}
+			else if (keyCode == 192){
+				chr = "`";
+			}
+			else if (keyCode == 219){
+				chr = "[";
+			}
+			else if (keyCode == 220){
+				chr = "\\";
+			}
+			else if (keyCode == 221){
+				chr = "]";
+			}
+			else if (keyCode == 222){
+				chr = "'";
+			}
+		}
+        _KernelInputQueue.enqueue(chr); 
+	}
 }
